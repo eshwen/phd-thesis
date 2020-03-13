22befc1e20ffe45d64cae5f94cf993346f46e100
Eshwen <eshwen.bhal@bristol.ac.uk>
2020-03-13 11:30:12 +0000

    Writing bash script to trash all auxiliary files instead of doing it manually

---
+++ trash_aux_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove all auxiliary files created by compiling thesis, including glossary and bibliography files
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for file in ${this_dir}/thesismain.*; do
+    if [[ $file == "${this_dir}/thesismain.tex" ]] || [[ $file == "${this_dir}/thesismain.pdf" ]]; then
+	continue
+    else
+	rm $file
+    fi
+done

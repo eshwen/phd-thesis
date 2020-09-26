5d88474d7a56622d8bb90378a8a03c057522a41a
eshwen <eshwen.bhal@bristol.ac.uk>
2020-09-26 18:50:19 +0100

    Fixing path to zip

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -32,4 +32,5 @@ tlmgr option -- autobackup 0
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
 
-unzip -d `kpsewhich --var-value TEXMFLOCAL` caption-CTAN_2020-07-20.zip
+pwd
+unzip -d `kpsewhich --var-value TEXMFLOCAL` ../texlive/caption-CTAN_2020-07-20.zip

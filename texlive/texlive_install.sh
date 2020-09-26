7c03c138db8a53a500d139160b1f632b0dd62507
eshwen <eshwen.bhal@bristol.ac.uk>
2020-09-26 18:58:13 +0100

    Actually fixing directory

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -32,5 +32,4 @@ tlmgr option -- autobackup 0
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
 
-pwd
-unzip -d `kpsewhich --var-value TEXMFLOCAL` ../texlive/caption-CTAN_2020-07-20.zip
+unzip -d `kpsewhich --var-value TEXMFLOCAL` ./texlive/caption-CTAN_2020-07-20.zip
